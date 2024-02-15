3bf0b950c81efe170fc69ae1e31e2d0e18a88c0b
Spirit Develop <betoespram1297@gmail.com>
2024-02-15 14:25:32 -0600

    🐛 FIX | PAGE

--- assets/js/correo.js
+++ assets/js/correo.js
@@ -1,11 +1,10 @@
 function sendEmail()   {  
-    console.log('entro');
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const mensaje = document.getElementById("massage").value;
-    if (name === '' || email === '' || phone === '' || mensaje === ''){
-        console.log('entro');
+    const empresa = document.getElementById("empresa").value;
+    if (!name && !email && !phone && !mensaje && !empresa) {
         const elemento = document.getElementById("notification");
         const title = document.getElementById("texto1");
         const menssage = document.getElementById("texto2");
@@ -14,48 +13,52 @@ function sendEmail()   {
         title.textContent="¡Tienes que llenar todos los campos para continuar!";
         menssage.textContent="Hubo un error al enviar el mensaje";
         $('.toast').toast('show')
-        setTimeout(function(){  $('.toast').toast('hide') }, 2500);
-    } else {
+        setTimeout(function(){  $('.toast').toast('hide') }, 1500);
+        return
+    } 
+    const elemento = document.getElementById("notification");
+    const title = document.getElementById("texto1");
+    const menssage = document.getElementById("texto2");
+    elemento.className = ''
+    elemento.className += "alert mb-0 alert-primary";
+    title.textContent="Tú mensaje se esta enviando ...";
+    menssage.innerHTML = '<div style=" text-align:center;" class="spinner-border text-primary" role="status"><span class="sr-only" style=" text-align:center;"></span></div>'
+    $('.toast').toast('show')
+    setTimeout(function(){  $('.toast').toast('hide') }, 1500);
+    var myHeaders = new Headers();
+    myHeaders.append("accept", "application/json");
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+    "name": name,
+    "email": email,
+    "phone":  phone,
+    "company":  empresa,
+    "description": mensaje,
+    "page": "axolotl"
+    });
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    fetch("https://axolotlcode.tech/api/send-email/", requestOptions)
+    .then(response => response.text())
+    .then(result => {
+        console.log(result);
         const elemento = document.getElementById("notification");
         const title = document.getElementById("texto1");
         const menssage = document.getElementById("texto2");
         elemento.className = ''
-        elemento.className += "alert mb-0 alert-primary";
-        title.textContent="Tú mensaje se esta enviando ...";
-        menssage.innerHTML = '<div style=" text-align:center;" class="spinner-border text-primary" role="status"><span class="sr-only" style=" text-align:center;"></span></div>'
+        elemento.className += "alert mb-0 alert-success";
+        title.textContent="¡Tú mensaje se envio correctamente!";
+        menssage.textContent="Pronto nos pondremos en contacto contigo.";
         $('.toast').toast('show')
         setTimeout(function(){  $('.toast').toast('hide') }, 1500);
-        const newEmial = {
-            Host:"smtp.gmail.com",
-            Username:"codeaxolotl@gmail.com",
-            Password:"kriboy21",
-            To:"soporte@axolotlcode.tech",
-            From:"codeaxolotl@gmail.com",
-            Subject:"Tenemos un nuevo cliente",
-            Body:`Información del cliente\n<br>Nombre del contacto : ${name},\n<br>Correo del contacto: ${email},\n<br>Telefono del contacto: ${phone},\n<br>Mensaje del contacto : ${mensaje}`
-        }
-        Email.send(newEmial).then(function(message){
-            if(message == 'OK'){
-                const elemento = document.getElementById("notification");
-                const title = document.getElementById("texto1");
-                const menssage = document.getElementById("texto2");
-                elemento.className = ''
-                elemento.className += "alert mb-0 alert-success";
-                title.textContent="¡Tú mensaje se envio correctamente!";
-                menssage.textContent="Pronto nos pondremos en contacto contigo.";
-                $('.toast').toast('show')
-                setTimeout(function(){  $('.toast').toast('hide') }, 2500);
-            } else {
-                    const elemento = document.getElementById("notification");
-                    const title = document.getElementById("texto1");
-                    const menssage = document.getElementById("texto2");
-                    elemento.className = ''
-                    elemento.className += "alert mb-0 alert-warning";
-                    title.textContent="¡Ups!";
-                    menssage.textContent="Hubo un problema al enviar el mensaje";
-                    $('.toast').toast('show')
-                    setTimeout(function(){  $('.toast').toast('hide') }, 2500);
-            }
-        });
-    }
+        return
+    })
+    .catch(error => console.log('error', error));
 }  
